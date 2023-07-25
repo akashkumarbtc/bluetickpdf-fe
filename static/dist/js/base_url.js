6ccbdac7dbc7dfaa0226eb1d0efed7c4a56212f2
Akash Kumar <88481845+akashkumarbtc@users.noreply.github.com>
2023-07-25 11:50:49 +0530

    Add files via upload

---
+++ static/dist/js/base_url.js
@@ -0,0 +1,8 @@
+var base_url = 'http://15.207.186.93'
+console.log(base_url)
+
+localStorage.setItem('baseURL',base_url);
+
+// http://127.0.0.1:8000
+// http://15.207.186.93/
+// https://erp-backend.edelassurance.com
