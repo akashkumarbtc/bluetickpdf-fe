71aecc0c0758cbba6a14a4e123c7809f364e7404
akashkumarbtc <akash.kumar@bluetickconsultants.com>
2024-03-14 20:41:58 +0530

    session restore

--- static/dist/js/base_url.js
+++ static/dist/js/base_url.js
@@ -1,9 +1,9 @@
-var base_url = 'http://127.0.0.1:5000'
+var base_url = 'https://coops-backend.bluetickconsultants.com/'
 var frontend_url = 'https://pdf.bluetickconsultants.com'
 console.log(base_url)
 
 localStorage.setItem('baseURL', base_url);
 
-// http://127.0.0.1:8000
+// http://127.0.0.1:5000
 // http://15.207.186.93/
 // https://coops-backend.bluetickconsultants.com
